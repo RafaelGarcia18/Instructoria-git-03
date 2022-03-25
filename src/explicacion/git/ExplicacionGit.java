6ad09d948b8f30ea8310b36a5303ebcd015a445f
Rafael Garcia <43767413+RafaelGarcia64@users.noreply.github.com>
2022-03-25 10:31:13 -0600

    Otro cambio

--- src/explicacion/git/ExplicacionGit.java
+++ src/explicacion/git/ExplicacionGit.java
@@ -2,6 +2,6 @@ package explicacion.git;
 public class ExplicacionGit {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println("mi otra cuenta estuvo aqui");
+        System.out.println("Cambi 2");
         }
 }
